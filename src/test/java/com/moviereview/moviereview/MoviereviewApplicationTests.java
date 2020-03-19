a16ade34c95c37837d66a7921b5d5aa32d1f41c4
Andrei Jugaurs <andreijugaurs@gmail.com>
2020-03-19 22:10:20 +0200

    first-commit

---
+++ src/test/java/com/moviereview/moviereview/MoviereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviereview.moviereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
