347241812dc930df09d572f3ad502fad5977e5ad
rober <rober@DESKTOP-65D7LP5>
2020-03-22 23:43:04 +0200

    dubla 2

---
+++ src/main/java/com/moviereview/moviereview/controller/AuthenticationController.java
@@ -0,0 +1,41 @@
+package com.moviereview.moviereview.controller;
+
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+//import com.webencyclop.moviereview.model.User;//model de user
+
+
+@RestController 
+public class AuthenticationController {
+
+	@RequestMapping(value = { "/login" }, method = RequestMethod.GET)
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login"); // resources/template/login.html
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public ModelAndView register() {
+		ModelAndView modelAndView = new ModelAndView();
+		// User user = new User();
+		// modelAndView.addObject("user", user); 
+		modelAndView.setViewName("register"); // resources/template/register.html
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("home"); // resources/template/home.html
+		return modelAndView;
+	}
+	  @RequestMapping("/")
+	  public String index() {
+	    return "login.html";
+	  }
+}
