a1f9fd143092a949ec676ac3c88777f796c48a6f
ValentinCerneanu <valentincerneanu28@gmail.com>
2020-03-29 22:58:14 +0300

    sqlScripts

---
+++ sqlScripts.sql
@@ -0,0 +1,15 @@
+Create database movie_review;
+
+use movie_review;
+
+create table users(
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(50),
+    username varchar(50),
+    password varchar(50),
+    status varchar(50),
+    PRIMARY KEY (id)
+);
+
+
+select * from users
