ad1f24c3a58293ae05668ddbe0a24ffa80141da9
jugaursa <jugaurs.andrei@cellwize.com>
2020-03-29 17:19:16 +0300

    Add fix to get beans at startup

--- src/main/java/com/moviereview/moviereview/MoviereviewApplication.java
+++ src/main/java/com/moviereview/moviereview/MoviereviewApplication.java
@@ -1,10 +1,12 @@
 package com.moviereview.moviereview;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class MoviereviewApplication {
 
