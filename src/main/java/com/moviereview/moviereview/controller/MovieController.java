31a6ba0662a23cc74cb9538aa5a266bb9610d1a4
ValentinCerneanu <valentincerneanu28@gmail.com>
2020-05-04 21:39:06 +0300

    Organize Imports

--- src/main/java/com/moviereview/moviereview/controller/MovieController.java
+++ src/main/java/com/moviereview/moviereview/controller/MovieController.java
@@ -1,17 +1,12 @@
 package com.moviereview.moviereview.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.moviereview.moviereview.model.Movie;
 import com.moviereview.moviereview.util.MovieApi;
