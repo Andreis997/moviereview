da3301f5830b4ff7ea380d5aadb3c375270f9907
Andrei <andreijugaurs@gmail.com>
2020-05-04 20:46:23 +0300

    Remove imports

--- src/main/java/com/moviereview/moviereview/MoviereviewApplication.java
+++ src/main/java/com/moviereview/moviereview/MoviereviewApplication.java
@@ -1,19 +1,10 @@
 package com.moviereview.moviereview;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Primary;
-
-import com.moviereview.moviereview.dao.ReviewDAO;
-import com.moviereview.moviereview.dao.UserDAO;
-import com.moviereview.moviereview.model.Review;
-import com.moviereview.moviereview.service.UserDetailsServiceImpl;
 
 @ComponentScan
 @SpringBootApplication
