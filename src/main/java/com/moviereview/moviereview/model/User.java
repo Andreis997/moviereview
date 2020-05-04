032c08364ea9168fd381129b0055d37526df48fe
Andrei <andreijugaurs@gmail.com>
2020-05-04 20:47:14 +0300

    Remove imports

--- src/main/java/com/moviereview/moviereview/model/User.java
+++ src/main/java/com/moviereview/moviereview/model/User.java
@@ -1,18 +1,13 @@
 package com.moviereview.moviereview.model;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -44,15 +39,11 @@ public class User {
 	@Column(name = "status")
 	private String status;
 
-	@OneToMany(mappedBy = "review", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
-	private Set<Review> reviews;
-
 	public User(int id, String username, String email, String password) {
 		this.id = id;
 		this.username = username;
 		this.email = email;
 		this.password = password;
-		this.reviews = new HashSet<Review>();
 	}
 
 	public User() {
@@ -102,15 +93,6 @@ public class User {
 		List<String> roles = new ArrayList<>();
 		roles.add("ADMIN");
 		return roles;
-	}
-	
-	public Set<Review> getReviews() {
-		return this.reviews;
-	}
-	
-	public void setReviews(Set<Review> reviews) {
-		this.reviews = reviews;
-		reviews.forEach(r -> r.setUser(this));
 	}
 
 }
